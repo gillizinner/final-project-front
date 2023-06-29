a51d5dd950d9c715e9c3acfccecef255e1b914f1
gillizinner <gili.zinner@gmail.com>
2023-06-29 22:09:00 +0300

    create event complete

--- src/comps_client/createEvent.js
+++ src/comps_client/createEvent.js
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react'
 import { AppContext } from '../appContext';
 import { useNavigate } from 'react-router';
+import { API_URL, doApiMethod } from '../services/apiService';
 
 export default function CreateEvent() {
-    const { selectedProffs } = useContext(AppContext);
-    const [message,setMessage]=useState("");
+    const { selectedProffs, location, event } = useContext(AppContext);
+    const [message, setMessage] = useState("");
     const nav = useNavigate();
-    const checkDates = () => {
+    const checkDates = async() => {
         let valid = true;
         let prevDate = selectedProffs[0]?.date;
         let i = 1
@@ -16,18 +17,42 @@ export default function CreateEvent() {
                 break;
             }
             prevDate = selectedProffs[i].date;
+            console.log("prev date")
+            console.log(prevDate)
             i++;
         }
         if (!valid) {
             setMessage("Choose same dates for all proffesionals")
         } else {
-            setMessage("Greate!")
+            let eventDetailes = await doCreateEventApi(prevDate);
+            nav('/client/eventComfirmation', { state: { eventDetailes } }); // Navigate to the new route with eventData as state
+        }
+    }
+
+    const doCreateEventApi = async (selectedDate) => {
+        let url = API_URL + "/events/";
+        let proffsArray = selectedProffs.map(item => item.proff);
+        try {
+            let body = {
+                type: event,
+                location: location,
+                date: selectedDate,
+                proffesionals: proffsArray
+            }
+            let resp = await doApiMethod(url, "POST", body);
+            console.log("event of client creator of event info:");
+            console.log(resp.data);
+            return resp.data;
+        }
+        catch (err) {
+            console.log(err);
+            alert("There problem try come back later");
         }
     }
     return (
         <>
             <button onClick={() => checkDates()} className='bg-success btn'>Create Event</button>
-            <br/>
+            <br />
             <br />
             <div>{message}</div>
         </>
