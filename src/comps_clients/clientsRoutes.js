80b4d52a4ce0c7ba096470b2d11e955613d1accd
tzipiMor <tzipi4040@gmail.com>
2023-06-30 15:48:42 +0300

    connect all files

--- src/comps_clients/clientsRoutes.js
+++ src/comps_clients/clientsRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 // import LoginUser from './loginUser'
 import ClientProfile from './clientProfile'
 import ClientsHome from './clientsHome'
@@ -9,21 +9,26 @@ import EventPicker from './eventPicker'
 import DatePicker from './datePicker'
 import EndMessage from './confirmMessage'
 import DisplayAvailableProffs from './displayAvailableProffs/displayAvailableProffs'
+import NavbarGeneralClient from '../comps_general/navbarGeneralClient'
 
 export const clientsRoutes = () => {
     return (
         <>
+            {/* <NavbarGeneralClient /> */}
+            {/* <Route path="/clients/*" element={<NavbarGeneralClient />} /> */}
+            <Route path="/client/*" element={<NavbarGeneralClient />} />
             {/* <Route path="/users/login" element={<LoginUser/>} /> */}
-            <Route path="/clients/clientProfile/:id" element={<ClientProfile/>} />
-            <Route path="/clients/home" element={<ClientsHome/>} />
-            <Route path="/clients/eventsClient" element={<EventsClient/>} />
-            <Route path="/clients/editClient/:id" element={<EditClient/>} />
+            <Route path="/clients/clientProfile/:id" element={<ClientProfile />} />
+            <Route path="/clients/home" element={<ClientsHome />} />
+            <Route path="/clients/eventsClient" element={<EventsClient />} />
+            <Route path="/clients/editClient/:id" element={<EditClient />} />
             <Route path="/client/eventpicker" element={<EventPicker />}></Route>
-            <Route path="/client/datepicker/:event/:location" element={<DatePicker/>}/>
+            <Route path="/client/datepicker/:event/:location" element={<DatePicker />} />
             <Route path="/client/eventComfirmation" element={<EndMessage />}></Route>
             <Route path="/client/showAvailableProffesionals" element={<DisplayAvailableProffs />} />
             {/* <Route path="/client/showAvailableProffesionalDates/:id" element={<DisplayAvailableDatesOfProff />} /> */}
             {/* <Route path="/users/userProfile/:id" element={<UserProfile/>} /> */}
+
         </>
     )
 }
